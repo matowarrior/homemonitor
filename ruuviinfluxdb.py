b704fa2925f2a299323f9ec1f00e13a2a32588cf
matowarrior <jari.hynninen@iki.fi>
2021-02-17 15:03:46 +0200

    Handle any number of sensors

--- ruuviinfluxdb.py
+++ ruuviinfluxdb.py
@@ -16,32 +16,22 @@ client = InfluxDBClient(url="http://localhost:8086", token="my-token", org="my-o
 write_api = client.write_api(write_options=SYNCHRONOUS)
 query_api = client.query_api()
 
+sensors = {}
+for sensor in range(len(macs)):
+    sensors[macs[sensor]] = locations[sensor] 
+print(sensors)
 
 #Get broadcast data
 datas = RuuviTagSensor.get_data_for_sensors(macs, timeout_in_sec)
-sensor1 = None
-sensor2 = None
-sensor3 = None
 print("Raw data")
-print(datas[macs[0]])
-print(datas[macs[1]])
-print(datas[macs[2]])
-try:
-    sensor1 = datas[macs[0]]
-except Exception:
-    pass
-try:
-    sensor2 = datas[macs[1]]
-except Exception:
-    pass
-try:
-    sensor3 = datas[macs[2]]
-except Exception:
-    pass
-i = -1
-for sensor in [sensor1, sensor2, sensor3]:
-    i+=1
-    write_api.write(database, "my-org", {"measurement": "ruuvitag",
-                                            "tags": {"location": locations[i],"mac": macs[i]},
-                                            "fields": {"humidity": sensor["humidity"], "temperature": sensor["temperature"],
-                                                       "pressure": sensor["pressure"], "battery": sensor["battery"]}})
+for sensor in range(len(macs)):
+    print(datas[macs[sensor]])
+
+for k,v in sensors.items():
+    try:
+        write_api.write(database, "my-org", {"measurement": "ruuvitag",
+                                            "tags": {"location": v,"mac": k},
+                                            "fields": {"humidity": datas[k]["humidity"], "temperature": datas[k]["temperature"],
+                                                       "pressure": datas[k]["pressure"], "battery": datas[k]["battery"]}})
+    except:
+        print(f"failed {v}")
